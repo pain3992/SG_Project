bae101dc39d4dd0be86779dd80cc693e803dbcf4
Bae Janghun <pain3992@naver.com>
2018-12-23 16:02:15 +0900

    12/23 test

--- app/src/main/java/com/graduate/seoil/sg_projdct/MainActivity.java
+++ app/src/main/java/com/graduate/seoil/sg_projdct/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    public int num1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
