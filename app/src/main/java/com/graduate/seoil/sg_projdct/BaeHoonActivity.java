50bb8b9e0d00e3ca703578e57dab97b2bf7d45cc
baehoons <qogns314@naver.com>
2019-01-26 15:58:50 +0900

    hoons project add

---
+++ app/src/main/java/com/graduate/seoil/sg_projdct/BaeHoonActivity.java
@@ -0,0 +1,13 @@
+package com.graduate.seoil.sg_projdct;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaeHoonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bae_hoon);
+    }
+}
