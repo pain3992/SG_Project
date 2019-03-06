8e383d3a5b02995b725159f1e00207662099507b
Bae Janghun <pain3992@naver.com>
2019-03-06 21:37:32 +0900

    SG백업2

---
+++ app/src/main/java/com/graduate/seoil/sg_projdct/GStartActivity.java
@@ -0,0 +1,49 @@
+package com.graduate.seoil.sg_projdct;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class GStartActivity extends AppCompatActivity {
+
+    Button login, register;
+
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gstart);
+
+//        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+//
+//        // Check if user is null
+//        if (firebaseUser != null) {
+//            Intent intent = new Intent(GStartActivity.this, IndexActivity.class);
+//            startActivity(intent);
+//            finish();
+//        }
+
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GStartActivity.this, LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GStartActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
