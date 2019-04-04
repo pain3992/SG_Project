6be9e93c09c0c53eec1220979e8dee459c952c04
Bae Janghun <pain3992@naver.com>
2019-04-04 15:52:51 +0900

    4월 4일

---
+++ app/src/main/java/com/graduate/seoil/sg_projdct/Adapter/PostAdapter.java
@@ -0,0 +1,7 @@
+package com.graduate.seoil.sg_projdct.Adapter;
+
+/**
+ * Created by baejanghun on 03/04/2019.
+ */
+public class PostAdapter {
+}
---
+++ app/src/main/java/com/graduate/seoil/sg_projdct/Model/Post.java
@@ -0,0 +1,7 @@
+package com.graduate.seoil.sg_projdct.Model;
+
+/**
+ * Created by baejanghun on 03/04/2019.
+ */
+public class Post {
+}
