ba4b6fa0383fd8b8116c73d5e3e23f7befbca645
Bae Janghun <pain3992@naver.com>
2019-04-11 16:29:13 +0900

    04/11 update

--- app/src/main/java/com/graduate/seoil/sg_projdct/Fragments/TimePickerFragment.java
+++ app/src/main/java/com/graduate/seoil/sg_projdct/Fragments/TimePickerFragment.java
@@ -18,12 +18,11 @@ public class TimePickerFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        Calendar calendar = Calendar.getInstance();
-        int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        int minute = calendar.get(Calendar.MINUTE);
-
-
-        return new TimePickerDialog(getActivity(), TimePickerDialog.THEME_HOLO_DARK, (TimePickerDialog.OnTimeSetListener) getActivity(), hour, minute, android.text.format.DateFormat.is24HourFormat(getActivity()));
+//        Calendar calendar = Calendar.getInstance();
+//        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+//        int minute = calendar.get(Calendar.MINUTE);
+        return new TimePickerDialog(getActivity(), TimePickerDialog.THEME_HOLO_DARK, (TimePickerDialog.OnTimeSetListener) getActivity(), 2, 0, true);
 //        return new TimePickerDialog(getActivity(), (TimePickerDialog.OnTimeSetListener) getActivity(), hour, minute, android.text.format.DateFormat.is24HourFormat(getActivity()));
     }
+
 }
